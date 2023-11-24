411497a122ce8d8097a0d0c78731b53b4db00bde
travelboys <151712640+travelboys@users.noreply.github.com>
2023-11-24 13:43:11 +0900

    Customers추가
    Co-Authored-By: travelboys <151712640+travelboys@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Les's develop management system!</h2>
-    </div>
-  );
+const customers = [{
+  'id': 1,
+  'image': 'https://placebear.com/640/360',
+  'name': '이민서',
+  'birthday': '890411',
+  'gender': '여자',
+  'job': '교사'
+},
+{
+  'id': 2,
+  'image': 'https://baconmockup.com/640/360',
+  'name': '오상진',
+  'birthday': '860103',
+  'gender': '남자',
+  'job': '백수'
+},
+{
+  'id': 3,
+  'image': 'https://placekitten.com/640/360',
+  'name': '심슨',
+  'birthday': '241010',
+  'gender': '여자',
+  'job': '애기'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id} 
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+
+    );
+  }
 }
 
 export default App;
